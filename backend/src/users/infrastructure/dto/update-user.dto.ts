1fd74fd4b725c3e90f106c1b402863f3f63bbc32
Malldonado <47877623+malldonado@users.noreply.github.com>
2025-01-20 08:56:31 -0300

    Delete backend directory

--- backend/src/users/infrastructure/dto/update-user.dto.ts
+++
@@ -1,8 +0,0 @@
-// Importa a função `PartialType` da biblioteca `@nestjs/mapped-types`
-import { PartialType } from '@nestjs/mapped-types'
-
-// Importa a classe `CreateUserDto` do arquivo correspondente
-import { CreateUserDto } from './create-user.dto'
-
-// Define a classe `UpdateUserDto` que estende `PartialType(CreateUserDto)`
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
