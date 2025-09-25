2e829446ae78206a7ee8034cc915ab5f26aa7310
Malldonado <47877623+malldonado@users.noreply.github.com>
2025-09-25 08:34:42 -0300

    Delete frontend directory

--- frontend/src/app/components/old-panel/tasks/active/button.tsx
+++
@@ -1,7 +0,0 @@
-"use client"
-
-const Button: React.FC = () => {
-    return (<button className="bg-[#315bd4] text-white font-bold w-[150px] h-[50px] ml-4 rounded-xl">Nova Tarefa</button>)
-}
-
-export default Button
